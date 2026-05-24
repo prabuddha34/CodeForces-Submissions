0fc10b5b13589f9b6b782b508cdf656cd6144a92
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-24 16:57:32 +0530

    Add Complicated GCD [A] from Codeforces

---
+++ 664/A - Complicated GCD/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        String a = sc.next();
+        String b = sc.next();
+ 
+        if (a.equals(b)) {
+            System.out.println(a);
+        } else {
+            System.out.println("1");
+        }
+ 
+        sc.close();
+    }
+}
