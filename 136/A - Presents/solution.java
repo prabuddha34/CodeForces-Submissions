ade86b0d86183f9fd9056edb576b3e8923e4b44b
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-24 16:54:27 +0530

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+ 
+        int n = scanner.nextInt();
+        int[] p = new int[n];
+        int[] ans = new int[n];
+ 
+        for (int i = 0; i < n; i++) {
+            p[i] = scanner.nextInt();
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            int receiver = p[i];
+            ans[receiver - 1] = i + 1; 
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
