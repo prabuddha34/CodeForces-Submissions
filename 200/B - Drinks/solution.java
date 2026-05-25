e0c2e490f3c7a8f59722b00e39c46ca64c4d11d8
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-25 10:48:50 +0530

    Add Drinks [B] from Codeforces

--- 200/B - Drinks/solution.java
+++ 200/B - Drinks/solution.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
  
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+ 
         Scanner sc = new Scanner(System.in);
  
-        int n = sc.nextInt();
-        double sum = 0;
+        int t = sc.nextInt();
  
-        for (int i = 0; i < n; i++) {
-            sum += sc.nextDouble(); 
+        int sum = 0;
+ 
+        for (int i = 0; i < t; i++) {
+            sum += sc.nextInt();
         }
  
-        double result = sum / n;
+        double percent = (double) sum / t;
  
-        System.out.printf("%.12f
-", result); 
+        System.out.println(percent);
+ 
+        sc.close();
     }
 }
