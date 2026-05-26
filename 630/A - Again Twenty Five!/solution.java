f9c054cb3a1cb336876a6fb4bccc4c47878e389f
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-26 16:30:56 +0530

    Add Again Twenty Five! [A] from Codeforces

--- 630/A - Again Twenty Five!/solution.java
+++ 630/A - Again Twenty Five!/solution.java
@@ -1,18 +1,12 @@
- 
 import java.util.Scanner;
  
 public class Main {
- 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong(); 
  
-        if (n == 0) {
-            System.out.println("01");
-        } else if (n == 1) {
-            System.out.println("05");
-        } else {
-            System.out.println("25");
-        }
+        Scanner sc = new Scanner(System.in);
+ 
+        long n = sc.nextLong();
+ 
+        System.out.println(25);
     }
 }
