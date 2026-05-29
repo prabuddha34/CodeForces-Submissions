7fd3244694e23ea27d34b3b1a5026efc55cf43eb
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-29 16:43:50 +0530

    Add Contest [A] from Codeforces

---
+++ 501/A - Contest/solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+ 
+        int misha = Math.max(3 * a / 10, a - (a / 250) * c);
+        int vasya = Math.max(3 * b / 10, b - (b / 250) * d);
+ 
+        if (misha > vasya) {
+            System.out.println("Misha");
+        } else if (vasya > misha) {
+            System.out.println("Vasya");
+        } else {
+            System.out.println("Tie");
+        }
+    }
+}
