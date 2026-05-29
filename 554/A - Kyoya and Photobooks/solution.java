c26468486c063b293cdd9e2dcbf054b8544d1933
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-05-29 17:24:49 +0530

    Add Kyoya and Photobooks [A] from Codeforces

---
+++ 554/A - Kyoya and Photobooks/solution.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println((name.length()+1)*25+1);
+    }
+}
