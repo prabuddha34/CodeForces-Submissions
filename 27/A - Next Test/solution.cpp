ec046c4f397e8de1c5fe8be6cd3813c1250372d4
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-20 15:17:46 +0530

    Add Next Test [A] from Codeforces

---
+++ 27/A - Next Test/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> a(n);
+ 
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    sort(a.begin(), a.end());
+ 
+    int ans = 1;
+ 
+    for(int i = 0; i < n; i++) {
+        if(a[i] == ans) {
+            ans++;
+        }
+    }
+ 
+    cout << ans << endl;
+ 
+    return 0;
+}
