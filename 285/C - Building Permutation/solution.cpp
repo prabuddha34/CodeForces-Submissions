6d73054f61fbff32522b0e41a8f20d29c1cc0e23
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-21 17:23:36 +0530

    Add Building Permutation [C] from Codeforces

---
+++ 285/C - Building Permutation/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<long long> a(n);
+ 
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    sort(a.begin(), a.end());
+ 
+    long long ans = 0;
+ 
+    for(int i = 0; i < n; i++) {
+        ans += abs(a[i] - (i + 1));
+    }
+ 
+    cout << ans << endl;
+ 
+    return 0;
+}
