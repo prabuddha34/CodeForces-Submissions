b15aab4ccadc055cfae2ff9b18cd007782d14b87
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-21 17:55:42 +0530

    Add Hungry Sequence [B] from Codeforces

---
+++ 327/B - Hungry Sequence/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    for(int i = 0; i < n; i++) {
+        cout << 5000001 + i << " ";
+    }
+ 
+    return 0;
+}
