f6addf1742750d243a051aa4a2901f206a5fad5e
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-21 11:45:27 +0530

    Add Tiling with Hexagons [A] from Codeforces

---
+++ 216/A - Tiling with Hexagons/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+ 
+    cout << a * b + b * c + c * a - a - b - c + 1 << endl;
+ 
+    return 0;
+}
