7e0cdffde40b0cf1d709b2e111e46dd7940067b4
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-22 10:39:35 +0530

    Add Soldier and Badges [B] from Codeforces

---
+++ 546/B - Soldier and Badges/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<int> a(n);
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    sort(a.begin(), a.end());
+ 
+    int cost = 0;
+ 
+    for (int i = 1; i < n; i++) {
+        if (a[i] <= a[i - 1]) {
+            int need = a[i - 1] + 1;
+            cost += need - a[i];
+            a[i] = need;
+        }
+    }
+ 
+    cout << cost << endl;
+ 
+    return 0;
+}
+ 
