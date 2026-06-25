80c3768bcde078b76aaa55b210b98eb15a32ad64
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-25 10:46:10 +0530

    Add Make It Good [C] from Codeforces

---
+++ 1385/C - Make It Good/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> a(n);
+ 
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+ 
+        int i = n - 1;
+ 
+        while (i > 0 && a[i - 1] >= a[i]) {
+            i--;
+        }
+ 
+        while (i > 0 && a[i - 1] <= a[i]) {
+            i--;
+        }
+ 
+        cout << i << endl;
+    }
+ 
+    return 0;
+}
