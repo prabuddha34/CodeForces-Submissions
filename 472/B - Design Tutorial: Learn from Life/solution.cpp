da25705222aa0a2090f04dc99156511291eeb4ba
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-27 11:26:12 +0530

    Add Design Tutorial: Learn from Life [B] from Codeforces

---
+++ 472/B - Design Tutorial: Learn from Life/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, k;
+    cin >> n >> k;
+ 
+    vector<int> f(n);
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> f[i];
+    }
+ 
+    sort(f.rbegin(), f.rend());
+ 
+    int ans = 0;
+ 
+    for (int i = 0; i < n; i += k) {
+        ans += 2 * (f[i] - 1);
+    }
+ 
+    cout << ans << '
+';
+ 
+    return 0;
+}
