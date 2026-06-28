de67c99c81fbd6a6c87e86c5a168a1be7d6d0317
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-06-28 16:19:43 +0530

    Add Sonya and Exhibition [B] from Codeforces

---
+++ 1004/B - Sonya and Exhibition/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+ 
+    for (int i = 0; i < m; i++) {
+        int l, r;
+        cin >> l >> r;
+    }
+ 
+    for (int i = 0; i < n; i++) {
+        cout << (i % 2);
+    }
+ 
+    cout << '
+';
+    return 0;
+}
