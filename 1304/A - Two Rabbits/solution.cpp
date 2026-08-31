00984e8ba95b8f744755ec87be2c856bfdb15fb9
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-08-31 23:09:44 +0530

    Add Two Rabbits [A] from Codeforces

---
+++ 1304/A - Two Rabbits/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int T;
+    cin >> T;
+ 
+    while (T--) {
+        long long x, y, a, b;
+        cin >> x >> y >> a >> b;
+ 
+        long long distance = y - x;
+        long long move = a + b;
+ 
+        if (distance % move == 0) {
+            cout << distance / move << '
+';
+        } else {
+            cout << -1 << '
+';
+        }
+    }
+ 
+    return 0;
+}
