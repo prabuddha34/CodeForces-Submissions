fd3d9db8f0be84fba313b64fd8f8a863190b5857
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-09-06 22:33:29 +0530

    Add In-game Chat [A] from Codeforces

---
+++ 1411/A - In-game Chat/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        string s;
+        cin >> s;
+ 
+        int cnt = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')') {
+                cnt++;
+            }
+            else {
+                break;
+            }
+        }
+ 
+        if (cnt > n - cnt)
+            cout << "Yes
+";
+        else
+            cout << "No
+";
+    }
+ 
+    return 0;
+}
