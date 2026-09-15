819de9ac78cd48562f506900e61174665c0449fd
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-09-15 21:38:08 +0530

    Add Kids Seating [A] from Codeforces

---
+++ 1443/A - Kids Seating/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        for (int i = 1; i <= n; i++) {
+            cout << 2 * n + 2 * i << " ";
+        }
+ 
+        cout << endl;
+    }
+ 
+    return 0;
+}
