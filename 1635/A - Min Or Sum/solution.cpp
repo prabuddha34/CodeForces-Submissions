e0e9f12f068ae246a2c34cfdf1955ff1a480f8b4
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-09-22 18:09:15 +0530

    Add Min Or Sum [A] from Codeforces

---
+++ 1635/A - Min Or Sum/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+       int n;
+        cin >> n;
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            long long x;
+            cin >> x;
+            ans|=x;
+ 
+        }
+        cout << ans << endl;
+    }
+ 
+    return 0;
+}
