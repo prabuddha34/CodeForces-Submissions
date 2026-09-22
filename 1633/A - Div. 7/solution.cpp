4caac19a036e01d134902a1ffb8dbe38cd2e8396
PrabuddhaPal <94966905+prabuddha34@users.noreply.github.com>
2026-09-22 17:56:40 +0530

    Add Div. 7 [A] from Codeforces

---
+++ 1633/A - Div. 7/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        if (n % 7 == 0) {
+            cout << n << endl;
+        }
+        else {
+            int ans = -1;
+ 
+            for (int j = 0; j < 10; j++) {
+                if ((n - n % 10 + j) % 7 == 0) {
+                    ans = n - n % 10 + j;
+                }
+            }
+ 
+            cout << ans << endl;
+        }
+    }
+ 
+    return 0;
+}
